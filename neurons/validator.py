68901c47a6249aadee48906025251fafa8b97537
dream-well <dream-well>
2024-03-17 03:01:26 -0700

    log running every 24s

--- neurons/validator.py
+++ neurons/validator.py
@@ -481,4 +481,4 @@ if __name__ == "__main__":
     with Validator() as validator:
         while True:
             bt.logging.info("Validator running...", time.time())
-            time.sleep(5)
+            time.sleep(24)
