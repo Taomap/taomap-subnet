0adba8c9065030937b8d00dc669a8f1b7da2231e
dream-well <dream-well>
2024-03-21 20:21:55 -0700

    change alpha to 0.7

--- taomap/utils/config.py
+++ taomap/utils/config.py
@@ -208,7 +208,7 @@ def add_validator_args(cls, parser):
         "--neuron.moving_average_alpha",
         type=float,
         help="Moving average alpha parameter, how much to add of the new observation.",
-        default=0.5,
+        default=0.7,
     )
 
     parser.add_argument(
